41e1f0a8b7a1673df34d5d6fab020f501cc7c390
Dayna Blackwell <daynajblackwell@gmail.com>
2017-12-26 07:44:56 -0700

    Added projects 4 - 9.

---
+++ computer science 1/BlackwellLab7.c
@@ -0,0 +1,64 @@
+//
+//
+// This program calculates the total amount that a rental car company should charge its customers.  
+// The user will enter the number of days the car will be rented, and how many miles the car will be driven.
+//  
+// A fixed rate of $15.00 is charged to all customers.  The rate for the miles driven is variable, depending on whether
+// the car was driven for less than 200 miles, effectively giving a slight discount on rentals greater than 200 miles.
+//
+//  A simple if-else structure determines which rate should be applied.
+//
+// The total cost to the customer is broken down into 3 components:  tax, some multiple of the daily rate and a charge based on the number of miles driven.
+// 
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+	int days = 0;														// integer value.  Number of days for rental.
+	double milesDriven = 0;												// floating point value holds the miles driven for the rental.
+
+	double salesTax = 0.06;												// floating point value holds the current sales tax percentage in decimal form.
+	double dailyRate = 15.00;											// floating point value holds the fixed daily rate.
+	double dailyTotal = 0;												// floating point value holds the product (days*dailyRate).
+	double milesCharge = 0;												// floating point value holds the cost component based on miles driven.
+	double taxCharge = 0;												// floating point value holds the sales tax assessed.
+	double subTotal = 0;												// floating point value holds the sum (dailyTotal + milesCharge)
+	double totalCost = 0;												// floating point value holds the final cost to the customer, which is the sum (subTotal+taxCharge)
+	
+																		// input/output for user values
+	puts("");
+	printf("%s", "Enter the number of days the car was rented:\t");
+	scanf("%d", &days);
+	printf("%s", "Enter the number of miles the car was driven:\t");
+	scanf("%lf", &milesDriven);
+	puts("");
+	
+	dailyTotal = days*dailyRate;								// This line satisfies the requirement that "the amount charged per 
+																// day for renting a car should only appear in one calculation."
+
+	if ( milesDriven < 200)										// if else structure multiples milesDriven by .4 for rentals less than 200mi, .35 if greater than or equal 200.
+	{
+		milesCharge = 0.40*milesDriven;
+	}
+	else
+	{
+		milesCharge = 0.35*milesDriven;
+	}
+	
+	subTotal = dailyTotal + milesCharge;						// Values to be used in final program output:  subTotal holds the sum of fixed daily charges and 
+																// variable miles charge.
+	
+	taxCharge = salesTax*(dailyTotal + milesCharge);			// holds the sales tax assessed to the customer
+	totalCost = (subTotal + taxCharge);							// holds the total cost to the customer.  Consists of the sum of subTotal and taxCharge
+	
+	printf("%s%.2lf","Subtotal:\t\t\t\t\t$",subTotal);			// output section for final values
+	puts("");
+	printf("%s%.2lf","Tax Amount:\t\t\t\t\t$",taxCharge );
+	puts("");
+	printf("%s%.2lf","Total:\t\t\t\t\t\t$",totalCost);
+	puts("");
+	
+	return 0;
+}
---
+++ computer science 1/BlackwellLab8.c
@@ -0,0 +1,68 @@
+// This program calculates how much a customer should be charged to board their dog at
+// a dog boarding business.  It calculates the daily charge based on the weight of the dog,
+// the number of days the dog will be boarded, and whether premium quality food has been chosen.
+//
+// There are two series of nested if-statements:  The outer if-statements sort input according 
+// to days-per-month, and the inner if-statements sort by dog weight.  The rate per day to the
+// customer will be determined based on which conditions are selected.
+//
+// After this, an additional rate is applied for cases where premium food has been selected.
+// The rate for premium food is a function of the dog's weight and will increase the total
+// rate per day by either 1 or 2 dollars per day.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+	int dogWeight;														// integer variable holds the weight of the dog
+	int daysPerMonth;													// integer variable holds the number of days per month the board will be boarded.
+	int isPremium;														// isPremium is an integer variable that will act as a flag to indicate whether premium food is selected.
+	double ratePerDay;													// floating point variable holds the daily rate to the customer.
+	//double monthlyCharges;
+	
+	puts("");															// input/output section for initial values.
+	printf("%s" , "How many pounds does your dog weigh?\t");
+	scanf("%d", &dogWeight);
+	printf("%s","How many days per month for boarding?\t");
+	scanf("%d",&daysPerMonth);
+	printf("%s" , "Premium food? (1 = yes or 2 = no)\t");
+	scanf("%d", &isPremium);
+	
+	if (daysPerMonth <= 10)												// if-structure for stays of 10 days or less:
+	{
+		if (dogWeight < 10) {ratePerDay = 12;}							// if dog weight is less than 10lb, $12.00 per day.
+		if (10 <= dogWeight < 35) {ratePerDay = 16;}					// if dog weight is greater than or equal 10lb AND less than 35lb, $16.00 per day.
+		if (dogWeight >= 35) {ratePerDay = 19;}							// if dog weight is greater than or equal 35lb, $19.00 per day.
+	}
+	
+	if (daysPerMonth >= 11)												// if-structure for stays of 11 days or more.
+	{
+		if (dogWeight < 10) {ratePerDay = 10;}							// if dog weight is less than 10lb, $10.00 per day.
+		if (dogWeight >= 10 && dogWeight < 35) {ratePerDay = 13;}		// if dog weight is greater than or equal 10lb AND less than 35lb, $13.00 per day.
+		if (dogWeight >= 35) {ratePerDay = 17;}							// if dog weight is greater than or equal 35lb, $17.00 per day.
+	}
+	
+	if ( (isPremium == 1) && (dogWeight <= 20) ) {ratePerDay+=1;}		// add 1 dollar to the rate if premium food is chosen and dog is less than or equal 20lb
+	if ( (isPremium == 1) && (dogWeight > 20) ) {ratePerDay+=2;}		// add 2 dollars to the rate if premium food is chosen and the dog is greater than 20lb.
+	
+	puts("");															// output section for final values
+	printf("%s%d%s" , "To board a ",dogWeight," pound dog for ");
+	
+	printf("%d%s%s", daysPerMonth, " days",
+		  ((isPremium == 1) ? " with premium food:" : ":"));			// uses a conditional statement within the printf argument to insert either "with premium
+		  																// food:" or ":"
+	
+	puts("");
+	printf("%s%.2lf","Rate per day:\t\t$",ratePerDay);
+	puts("");
+	printf("%s%.2lf","Monthly charges:\t$",ratePerDay*daysPerMonth);	// Total monthly charges are calculated by ratePerDay*daysPerMonth.
+	puts("");
+	
+	return 0;
+}
+
+
+
+
+
