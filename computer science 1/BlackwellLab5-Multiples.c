147e1ef7cb55f9ef5f1ff8c0b1b23771e659bb4a
Dayna Blackwell <daynajblackwell@gmail.com>
2017-12-26 17:27:14 -0700

    Update BlackwellLab5-Multiples.c

--- computer science 1/BlackwellLab5-Multiples.c
+++ computer science 1/BlackwellLab5-Multiples.c
@@ -1,12 +1,12 @@
 // Program uses 2 counter-controlled for loops in order to print the first 5 multiples of each of the first 10 integers.
 // Outer for-loop iterates by row.  Inner for-loop iterates by column.
 
-#include <stdio.h>											// include preproc. directive makes contents of stdio available to this file.
+#include <stdio.h>							// include preproc. directive makes contents of stdio available to this file.
 
 int main(void)
 {
-	unsigned int rowCounter;								// row counter for counter controlled for-loop
-	unsigned int colCounter;								// column counter for counter controlled for-loop
+	unsigned int rowCounter;					// row counter for counter controlled for-loop
+	unsigned int colCounter;					// column counter for counter controlled for-loop
 	
 	puts("");
 	printf("%s","Number\t1st\t2nd\t3rd\t4th\t5th");			// outputs header.
@@ -14,12 +14,12 @@ int main(void)
 	
 	for (rowCounter = 1; rowCounter <=10; ++rowCounter)		// for loop iterates through values 1 to 10, incrementing by 1 on each pass.
 	{
-		printf("%u",rowCounter);							// rowCounter is used to print each of the first 10 integer values.
+		printf("%u",rowCounter);				// rowCounter is used to print each of the first 10 integer values.
 	
 		for (colCounter = 1; colCounter <=5; ++colCounter)	// for loop interates through the values 1 - 5.
 		{
-			printf("\t%u",rowCounter*colCounter);			// Each initial row value is multipled by the numbers 1 - 5 in turn, forming the first 5 multiples
-		}													// and the columns of the output table.
+			printf("\t%u",rowCounter*colCounter);		// Each initial row value is multipled by the numbers 1 - 5 in turn, forming the first 5 multiples
+		}							// and the columns of the output table.
 		
 		puts("");
 	}
