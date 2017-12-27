d5e591083acbb3163c5ce5853519b5608b686263
Dayna Blackwell <daynajblackwell@gmail.com>
2017-12-26 17:34:14 -0700

    Update BlackwellLab9.c

--- computer science 1/BlackwellLab9.c
+++ computer science 1/BlackwellLab9.c
@@ -2,12 +2,12 @@
 
 #include <stdio.h>
 
-double kilToMiles(int);													// function prototype for kilometer to miles conversions.  Accepts int, returne double.
-double milesToKil(int);													// function prototype for miles to kilometers conversion.  Accepts int, returns double.
+double kilToMiles(int);								// function prototype for kilometer to miles conversions.  Accepts int, returne double.
+double milesToKil(int);								// function prototype for miles to kilometers conversion.  Accepts int, returns double.
 
 int main (void)
 {
-	int x;																// declare int for use as control variable in for loop.
+	int x;									// declare int for use as control variable in for loop.
 	
 	puts("");
 	printf("%s", "Kilometers per hour converted to miles per hour: ");
@@ -15,9 +15,9 @@ int main (void)
 	
 	printf("%s\t%s\n","Kph", "Mph");
 	
-	for (x=185; x >= 0; x-=5)											// counter controlled loop is used to pass arguments into the conversion functions
+	for (x=185; x >= 0; x-=5)						// counter controlled loop is used to pass arguments into the conversion functions
 	{
-		printf("%i\t%.0lf\n",x,kilToMiles(x));							// kilToMiles function is called within the argument list of printf. 
+		printf("%i\t%.0lf\n",x,kilToMiles(x));				// kilToMiles function is called within the argument list of printf. 
 	}																	// conversion function is passed control variable as an argument.
 	
 	puts("");
@@ -33,16 +33,15 @@ int main (void)
 	return 0;
 }
 
-double kilToMiles(int kilometers)										// full kilometer to miles function definition.
+double kilToMiles(int kilometers)						// full kilometer to miles function definition.
 {
-	return kilometers*0.621371;											// returns kilometers multiplied by the appropriate conversion factor, returning
+	return kilometers*0.621371;						// returns kilometers multiplied by the appropriate conversion factor, returning
 																		// a value in miles
 	
 }
 
-double milesToKil(int miles)											// full miles to kilometer function defintion.
+double milesToKil(int miles)							// full miles to kilometer function defintion.
 {
-	return miles*1.60934;												// returns miles multiplied by the appropriate conversion factor, returning
-																		// a value in kilometers.
-	
+	return miles*1.60934;							// returns miles multiplied by the appropriate conversion factor, returning
+										// a value in kilometers.
 }
